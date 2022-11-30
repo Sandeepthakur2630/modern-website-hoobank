db1321c96fde646f8ac90f3e50c81107b91e7c82
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-11-30 13:43:43 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,11 +48,9 @@ const Hero = () => {
           className="w-[100%] h-[100%] relative z-[5]"
         />
 
-        {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-        {/* gradient end */}
       </div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
