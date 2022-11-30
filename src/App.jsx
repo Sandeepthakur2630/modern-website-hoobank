ec7fce04d988c131920007f47114986df32c0a21
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-11-30 13:20:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./styles";
+import { Navbar, Hero } from "./components";
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        stats business billing carddeal testimonials clients cta footer
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
