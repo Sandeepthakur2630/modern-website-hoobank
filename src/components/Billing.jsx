fe384a4db630a6934e650f2d871b01fea3427edb
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-11-30 17:44:33 +0530

    added the billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
-  )
-}
+    <div className="flex text-white">
+      <div></div>
+      <div>
+        <h1>Easily control your billing & invoicing</h1>
+        <p>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div>
+          <img src="" alt="aap store" />
+          <img src="" alt="google play" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Billing
+export default Billing;
