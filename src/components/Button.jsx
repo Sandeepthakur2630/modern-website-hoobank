9c8a9720fb87ee815991c1f2c85cead4e986a6cb
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-12-01 16:43:42 +0530

    1dec commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Button = ({ title }) => {
-  return <div className="bg-white ">{title}</div>;
-};
+const Button = ({ styles }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
+    Get Started
+  </button>
+);
 
 export default Button;
