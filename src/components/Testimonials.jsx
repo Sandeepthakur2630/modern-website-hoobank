da5bb5541e0aed5540b89f15fc9ddee3a9e0abf5
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-12-01 18:03:11 +0530

    hello

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,68 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div className="bg-white">Testimonials</div>;
+  return (
+    <div className="bg-white">
+      <div className="flex items-center">
+        <div>What people are saying about us</div>
+        <p>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+
+      <div className="flex">
+        <div>
+          <img src="" alt="image" />
+          <p>
+            Money is only a tool. It will take you wherever you wish, but it
+            will not replace you as the driver.
+          </p>
+          <div className="flex">
+            <div>
+              <img src="" alt="profilepic" />
+            </div>
+            <div>
+              <p>Harman jonson</p>
+              <p>Founder & Leader</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="image" />
+          <p>
+            Money makes your life easier. If you're lucky to have it, you're
+            lucky.
+          </p>
+          <div className="flex">
+            <div>
+              <img src="" alt="profilepic" />
+            </div>
+            <div>
+              <p>Steve Mark</p>
+              <p>Founder & Leader</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="image" />
+          <p>
+            It is usually people in the money business, finance, and
+            international trade that are really rich.
+          </p>
+          <div className="flex">
+            <div>
+              <img src="" alt="profilepic" />
+            </div>
+            <div>
+              <p>Ken Gallagher</p>
+              <p>Founder & Leader</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
